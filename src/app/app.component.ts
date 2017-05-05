e60ad31781e66d4ab28e55b447a82a81394ddbd6
alex <alex@lcnet.com.tw>
2017-05-05 22:01:37 +0800

    初始化row& column

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  board = [];
+
+  setBoard(): void {
+    this.board = [];
+
+    for (var i = 0; i < 16; i++) {
+      this.board[i] = [];
+      for (var j = 0; j < 16; j++) {
+        this.board[i][j] = false;
+      }
+    }
+  }
+  ngOnInit() {
+    this.setBoard();
+  }
 }
