3df4e9c1d48a906aa57c3625e244a02ced7e4bdd
dbqp9876 <dbqp9876@gmail.com>
2017-05-05 23:39:33 +0900

    新增按鍵事件& 解決衝突

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Controls } from './app.consts';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  host: {
+    '(document:keydown)': 'handleKeyboardEvents($event)'
+  }
 })
 export class AppComponent implements OnInit {
+
   board = [];
+  EventDirect: number;
+
+
+  ngOnInit() {
+    this.setBoard();
+  }
 
   setBoard(): void {
     this.board = [];
@@ -18,7 +29,17 @@ export class AppComponent implements OnInit {
       }
     }
   }
-  ngOnInit() {
-    this.setBoard();
+
+  handleKeyboardEvents(e: KeyboardEvent) {
+    if (e.keyCode === Controls.RIGHT) {
+      this.EventDirect = Controls.RIGHT;
+    } else if (e.keyCode === Controls.LEFT) {
+      this.EventDirect = Controls.LEFT;
+    } else if (e.keyCode === Controls.UP) {
+      this.EventDirect = Controls.UP;
+    }
+    else if (e.keyCode === Controls.DOWN) {
+      this.EventDirect = Controls.DOWN;
+    }
   }
 }
