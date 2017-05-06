9077b2693b9f1a3066a11fc4d249dcca8b161564
dbqp9876 <dbqp9876@gmail.com>
2017-05-06 20:59:51 +0900

    增加蛇

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Controls } from './app.consts';
+import { Controls, Colors } from './app.consts';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +12,18 @@ import { Controls } from './app.consts';
 export class AppComponent implements OnInit {
 
   board = [];
-  EventDirect: number;
+  Snake = {
+    direction: Controls.RIGHT,
+    position: [
+      [0, 0], [0, 1], [0, 2]
+    ]
+  };
+  interval: Number = 150;
 
 
   ngOnInit() {
     this.setBoard();
+    this.setSnakePosition();
   }
 
   setBoard(): void {
@@ -32,14 +39,70 @@ export class AppComponent implements OnInit {
 
   handleKeyboardEvents(e: KeyboardEvent) {
     if (e.keyCode === Controls.RIGHT) {
-      this.EventDirect = Controls.RIGHT;
+      this.Snake.direction = Controls.RIGHT;
     } else if (e.keyCode === Controls.LEFT) {
-      this.EventDirect = Controls.LEFT;
+      this.Snake.direction = Controls.LEFT;
     } else if (e.keyCode === Controls.UP) {
-      this.EventDirect = Controls.UP;
+      this.Snake.direction = Controls.UP;
     }
     else if (e.keyCode === Controls.DOWN) {
-      this.EventDirect = Controls.DOWN;
+      this.Snake.direction = Controls.DOWN;
     }
   }
+
+
+  setSnakeColor(x: Number, y: Number): string {
+
+    for (let i = 0; i < this.Snake.position.length; i++) {
+      if (x == this.Snake.position[i][0] && y == this.Snake.position[i][1]) {
+        if (i == this.Snake.position.length - 1) return Colors.snake_head;
+        return Colors.snake_body;
+      };
+    }
+    return Colors.background;
+  }
+
+  setSnakePosition() {
+    let tempArray = [];
+    switch (this.Snake.direction) {
+      case Controls.LEFT:
+        if (this.Snake.position[this.Snake.position.length - 1][1] - 1 >= 0) {
+          tempArray.push(this.Snake.position[this.Snake.position.length - 1][0]); //x
+          tempArray.push(this.Snake.position[this.Snake.position.length - 1][1] - 1); //y
+          this.Snake.position.push(tempArray);
+          this.Snake.position.shift(); //移除第一個值
+        }
+        break;
+      case Controls.RIGHT:
+        if (this.Snake.position[this.Snake.position.length - 1][1] + 1 < this.board.length) {
+          tempArray.push(this.Snake.position[this.Snake.position.length - 1][0]); //x
+          tempArray.push(this.Snake.position[this.Snake.position.length - 1][1] + 1); //y
+          this.Snake.position.push(tempArray);
+          this.Snake.position.shift(); //移除第一個值
+        }
+        break;
+      case Controls.UP:
+        if (this.Snake.position[this.Snake.position.length - 1][0] - 1 >= 0) {
+          tempArray.push(this.Snake.position[this.Snake.position.length - 1][0] - 1); //x
+          tempArray.push(this.Snake.position[this.Snake.position.length - 1][1]); //y
+          this.Snake.position.push(tempArray);
+          this.Snake.position.shift(); //移除第一個值
+        }
+        break;
+      case Controls.DOWN:
+        if (this.Snake.position[this.Snake.position.length - 1][0] + 1 < this.board[0].length) {
+          tempArray.push(this.Snake.position[this.Snake.position.length - 1][0] + 1); //x
+          tempArray.push(this.Snake.position[this.Snake.position.length - 1][1]); //y
+          this.Snake.position.push(tempArray);
+          this.Snake.position.shift(); //移除第一個值
+        }
+        break;
+    }
+
+    setTimeout(() => {
+      this.setSnakePosition();
+    }, this.interval);
+    
+  }
+
 }
--- src/app/app.consts.ts
+++ src/app/app.consts.ts
@@ -1,6 +1,12 @@
 export const Controls = {
     LEFT: 37,
-    RIGHT : 39 ,
+    RIGHT : 39,
     UP : 38,
     DOWN: 40
+}
+
+export const Colors = {
+    background : 'cadetblue',
+    snake_head : 'red',
+    snake_body : 'yellow'
 }
